d098077134f5344e186ec50c3a7de631e7c978dd
Angelica Aguilar <isaj44631@gmail.com>
2025-10-22 22:34:35 -0600

    Agregar funcion de abrir archivo y crear salida; limpiar todo los signos

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-//primer commit
+int main(int argumento, char *argumento[]) {
+	if (argumento != 4) {
+		printf("Uso: %s <archivo_entrada> <palabra_buscar> <reemplazo>\n", argumento[0]);
+		return 1;
+	}
+
+	FILE *entra = fopen(argumento[1], "r");
+	if (!entra) {
+		printf("Error: no se puedo abrir.\n");
+		return 1;
+	}
+
+
+	FILE *sal = fopen("reemplazo.txt", "w");
+	if (!sal) {
+		printf("Error: no se pudo crear el archivo.\n");
+		fclose(entra);
+		return 1;	
+	}
+
+	char wrd[100];
+	char limpiar[100];
+	char *buscar = argumento[2];
+	char *reemplazo = argumento[3];
+
+	while (fscanf(entra, "%s", wrd) == 1) {
+		strcpy(limpiar, wrd);
+		int len = strlen(limpiar);
+		if (limpiar[len - 1] == '.' || limpiar[len - 1] == ',') {
+			limpiar[len - 1] = '/0';
+		}
 
